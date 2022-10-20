8afbb0b5df1cba39b884f90d714a0e3ec76110a5
Teemotroll2 <gofoufigamer@gmail.com>
2022-10-20 01:58:26 -0500

    Se finaliza la primera parte a la 1:58 am :(

--- app/src/main/java/com/example/buyandsellricofinal/MainActivity.java
+++ app/src/main/java/com/example/buyandsellricofinal/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.buyandsellricofinal;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +18,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
@@ -27,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String BaseDeDatosRico;
-
+        final String[] IdSeller = new String[1];
         EditText email = findViewById(R.id.MainEmail);
         EditText name = findViewById(R.id.MainName);
         EditText phone = findViewById(R.id.MainPhone);
@@ -36,59 +39,201 @@ public class MainActivity extends AppCompatActivity {
         Button botonsearch = findViewById(R.id.btnsearch);
         Button botonedit = findViewById(R.id.btnedit);
         Button botondelete = findViewById(R.id.btndelete);
+        Button botonsales = findViewById(R.id.btnsales);
         EditText TotalComiciones = findViewById(R.id.MainTotalComision);
 
 
+        //IR A LA OTRA PAGINA
+        botonsales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SellRico.class));
+            }
+        });
+
+        //BOTON BORRAR
+        botondelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    // Confirmación de borrado
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertDialogBuilder.setMessage("¿ Está seguro de eliminar el cliente con Id: " + email.getText().toString() + " ?");
+                    alertDialogBuilder.setPositiveButton("Sí",
+                            new DialogInterface.OnClickListener() {
+
+
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+                                    // Se eliminará el cliente con el id respectivo
+
+                                    db.collection("Seller").document(IdSeller[0])
+                                            .delete()
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(MainActivity.this, "Cliente borrado correctamente...", Toast.LENGTH_SHORT).show();
+
+                                                    //Limpiar las cajas de texto
+                                                    email.setText("");
+                                                    name.setText("");
+                                                    phone.setText("");
+                                                    TotalComiciones.setText("");
+                                                    email.requestFocus(); //Enviar el foco al ident
+                                                }
+                                            })
+
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+                                }
+                            });
+
+                    alertDialogBuilder.setNegativeButton("No",
+                            new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+
+                                }
+                            });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+
+            }
+        });
+
+
+        //BOTON EDITARRRRRR
+        botonedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editarCustomer(email.getText().toString(), name.getText().toString(), phone.getText().toString(), TotalComiciones.getText().toString());
+            }
+
+            //Boton Editar Seller
+            private void editarCustomer(String semail, String sname, String sphone, String sTotalComiciones) {
+                Map<String, Object> mcustomer = new HashMap<>();
+                mcustomer.put("email", semail);
+                mcustomer.put("name", sname);
+                mcustomer.put("phone", sphone);
+                mcustomer.put("TotalComiciones", sTotalComiciones);
+
+                db.collection("Seller").document(IdSeller[0])
+                        .set(mcustomer)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this,"Cliente actualizado correctmente...",Toast.LENGTH_SHORT).show();
+
+                                // Vaciar las cajas de texto
+                                email.setText("");
+                                name.setText("");
+                                phone.setText("");
+                                TotalComiciones.setText("");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+              }});
+            }
+        });
+
+        //BOTON CLICK EN SAVE
         botonsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SaveSeller(email.getText().toString(), name.getText().toString(), phone.getText().toString(),TotalComiciones.getText().toString());
             }
 
-        });
+            // LO QUE HACE EL BOTON SAVE
+            private void SaveSeller(String semail, String sname, String sphone, String sTotalComiciones) {
+                // buscar la identificacion del cliente nuevo
+                db.collection("Seller")
+                        .whereEqualTo("email", semail)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (task.getResult().isEmpty()) { // no encontro el documento, se puede guardar xd ( ni idea)
+                                        //GUARDAR LOS DATOS DEL CLIENTE AYUDAA
+                                        Map<String, Object> customer = new HashMap<>(); // Tabla cursor
+                                        customer.put("email", semail);
+                                        customer.put("name", sname);
+                                        customer.put("phone", sphone);
+                                        customer.put("TotalComiciones", sTotalComiciones);
 
-    }
-
-        private void SaveSeller(String semail, String sname, String sphone, String sTotalComiciones) {
-            // buscar la identificacion del cliente nuevo
-            db.collection("Seller")
-                    .whereEqualTo("email", semail)
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                if (task.getResult().isEmpty()) { // no encontro el documento, se puede guardar xd ( ni idea)
-                                    //GUARDAR LOS DATOS DEL CLIENTE AYUDAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                                    Map<String, Object> customer = new HashMap<>(); // Tabla cursor
-                                    customer.put("email", semail);
-                                    customer.put("name", sname);
-                                    customer.put("phone", sphone);
-                                    customer.put("TotalComitions", sTotalComiciones);
-
-                                    db.collection("Seller")
-                                            .add(customer)
-                                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                                @Override
-                                                public void onSuccess(DocumentReference documentReference) {
-                                                    //Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
-                                                    Toast.makeText(getApplicationContext(), "Cliente agregado correctamente...",Toast.LENGTH_SHORT).show();
-                                                }
-                                            })
-                                            .addOnFailureListener(new OnFailureListener() {
-                                                @Override
-                                                public void onFailure(@NonNull Exception e) {
-                                                    Toast.makeText(getApplicationContext(), "Error! Cliente no se guardó...",Toast.LENGTH_SHORT).show();
-                                                }
-                                            });
-                                }
-                                else
-                                {
-                                    Toast.makeText(getApplicationContext(),"ID de el cliente EXISTENTE ♥♥♥",Toast.LENGTH_SHORT).show();
+                                        db.collection("Seller")
+                                                .add(customer)
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+                                                        //Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                                        Toast.makeText(getApplicationContext(), "Cliente agregado correctamente...",Toast.LENGTH_SHORT).show();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(getApplicationContext(), "Error! Cliente no se guardó...",Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(),"ID de el cliente EXISTENTE ♥♥♥",Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                             }
-                        }
-                    });
-            /*  */
-        }
+                        });
+                /*  */
+
+            }
+        });
+
+        //SE PONE EL CLICK EN EL BOTON BUSCAR
+        botonsearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchCustomer(email.getText().toString());
+            }
+
+            private void searchCustomer(String semail) {
+                db.collection("Seller")
+                        .whereEqualTo("email", semail)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (!task.getResult().isEmpty()) { // Si encontró el documento
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            IdSeller[0] = document.getId();
+                                            name.setText(document.getString("name"));
+                                            phone.setText(document.getString("phone"));
+                                            TotalComiciones.setText(document.getString("TotalComiciones"));
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(getApplicationContext(),"El Id del cliente no existe...",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+            }
+
+        });
+
+
+
     }
+}
--- app/src/main/java/com/example/buyandsellricofinal/SellRico.java
+++ app/src/main/java/com/example/buyandsellricofinal/SellRico.java
@@ -1,14 +1,87 @@
 package com.example.buyandsellricofinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SellRico extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_rico);
+
+        EditText salesemail = findViewById(R.id.etSalesEmail);
+        EditText salesdate = findViewById(R.id.etSalesDate);
+        EditText salesvalue = findViewById(R.id.etSalesValue);
+        Button botonguardar = findViewById(R.id.btnguardarsales);
+
+
+        //BOTON CLICK EN SAVE
+        botonguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveSeller(salesemail.getText().toString(), salesdate.getText().toString(), salesvalue.getText().toString());
+            }
+
+
+            // LO QUE HACE EL BOTON SAVE
+            private void SaveSeller(String ssalesemail, String ssalesdate, String ssalesvalue) {
+                // buscar la identificacion del cliente nuevo
+                db.collection("Sales")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (!task.getResult().isEmpty()) { // no encontro el documento, se puede guardar xd ( ni idea)
+                                        //GUARDAR LOS DATOS DEL CLIENTE AYUDAA
+                                        Map<String, Object> customer = new HashMap<>(); // Tabla cursor
+                                        customer.put("salesemail", ssalesemail);
+                                        customer.put("salesdate", ssalesdate);
+                                        customer.put("salesvalue", ssalesvalue);
+
+
+                                        db.collection("Sales")
+                                                .add(customer)
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+                                                        //Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                                        Toast.makeText(getApplicationContext(), "Cliente agregado correctamente...", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(getApplicationContext(), "Error! Cliente no se guardó...", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "ID de el cliente EXISTENTE ♥♥♥", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
